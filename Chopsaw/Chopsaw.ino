35683216ecbfa9be41ea98610e0a5d597e6650db
Jeffrey Lipton <jeffreyilipton@gmail.com>
2017-08-31 12:46:09 -0400

    fixed settings

--- Chopsaw/Chopsaw.ino
+++ Chopsaw/Chopsaw.ino
@@ -1,8 +1,10 @@
 
+/// A
 int PowerTail_D = 12;
 int PowerTail_pwm = 3;
 int PowerTail_break = 9;
 
+//B
 int Motor_D = 13;
 int Motor_pwm = 11;
 int Motor_break = 8;
@@ -14,7 +16,11 @@ int Motor_pot = A0;
 
 int target = 0;
 int val = 0;
-const float A = 255.0 / 1024.0;
+//const float A = 255.0 / 1024.0;
+int open = 29;
+int closed = 40;
+bool startstop = false;
+
 
 void setup()
 {
@@ -30,7 +36,7 @@ void setup()
 	digitalWrite(PowerTail_D, HIGH);
 	digitalWrite(Motor_D, HIGH);
 	digitalWrite(PowerTail_break, LOW);
-	digitalWrite(Motor_break, LOW);
+	digitalWrite(Motor_break, HIGH);
 }
 
 
@@ -42,48 +48,69 @@ void loop()
 		char c = Serial.read();
 		if (c == 'o') 
 		{
-			Serial.print("Opening");
-			target = 1000;
+			Serial.print("o");
+			target = open;
+			startstop = true;
 		}
 		else if (c == 'c') 
 		{
-			Serial.print("Closing");
-			target = 100;
+			Serial.print("c");
+			target = closed;
+			startstop = true;
+		}
+		else if (c == 's')
+		{
+			Serial.print("s");
+			target = val;
+			startstop = false;
+			digitalWrite(Motor_pwm, LOW);
 		}
 		else if (c == 'b')
 		{
-			Serial.print("Blade Starting");
+			Serial.print("b");
 			digitalWrite(PowerTail_pwm, HIGH);
 		}
-		else if (c == 'b')
+		else if (c == 'B')
 		{
-			Serial.print("Blade Stoping");
+			Serial.print("B");
 			digitalWrite(PowerTail_pwm, LOW);
 		}
 	}
-	MotorController();
+	if (startstop) { MotorController(); }
 
 }
 
 
 void MotorController() 
 {
-	val = analogRead(Motor_pot)/1023;
+	val = analogRead(Motor_pot);
 	int  delta = target - val;
 	int delta_abs = abs(delta);
 	int pwm = 0;
 
 	if (delta > 0) 
 	{
-		digitalWrite(Motor_D, HIGH);
+		digitalWrite(Motor_D, LOW);
 	}
 	else 
 	{
-		digitalWrite(Motor_D, LOW);
+		digitalWrite(Motor_D, HIGH);
 	}
 
-	if (delta_abs > 50) {
-		pwm = int(A*delta_abs);
+	if ( (delta_abs > 1) && ( (val>open) || (val <closed) ) )
+	{
+		pwm = 255;
+		digitalWrite(Motor_pwm, 255);
+		digitalWrite(Motor_break, LOW);
 	}
-	analogWrite(Motor_pwm, pwm);
+	else 
+	{
+		startstop = false;
+		Serial.print("s");
+		digitalWrite(Motor_pwm, 0);
+		digitalWrite(Motor_break, HIGH);
+	}
+
+	//Serial.print("read: ");
+	//Serial.println(val);
 }
